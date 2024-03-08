448f65dd5ee1f0adfff52918472db8924f610f2e
Lucas Fraporti <80408351+lucasfraporti@users.noreply.github.com>
2024-03-07 23:03:49 -0300

    added mock: sign in

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,7 +2,9 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
-export const worker = setupWorker()
+import { signInMock } from './sign-in-mocks'
+
+export const worker = setupWorker(signInMock)
 
 export const enableMSW = async () => {
   if (env.MODE !== 'test') {
