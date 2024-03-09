359571d7da9af8a4432aa7e7b3f529a599ae2907
Lucas Fraporti <80408351+lucasfraporti@users.noreply.github.com>
2024-03-08 21:13:17 -0300

    added e2e test: sign in

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+  await page.getByLabel('Seu e-mail').fill('lucasmfraporti@gmail.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText(
+    'Enviamos um link de autenticação para o seu e-mail.',
+  )
+
+  expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+  await page.getByLabel('Seu e-mail').fill('wrong@wrong.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas.')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+  await page.getByRole('button', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
